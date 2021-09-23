dd015d059e67d0fcb68c158c7a5f2bbaf4285136
Adadulla Hil Galib <coderboygalif@gmail.com>
2021-09-23 13:23:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var btn = document.querySelectorAll('.btn')
+var slider = document.querySelector('.slider')
+
+btn[0].onclick =  function () {
+    slider.style.transform = 'translateX(0px)';
+}
+btn[1].onclick =  function () {
+    slider.style.transform = 'translateX(-300px)';
+}
+btn[2].onclick =  function () {
+    slider.style.transform = 'translateX(-600px)';
+}
+btn[3].onclick =  function () {
+    slider.style.transform = 'translateX(-900px)';
+}
+btn[4].onclick =  function () {
+    slider.style.transform = 'translateX(-1200px)';
+}
+btn[5].onclick =  function () {
+    slider.style.transform = 'translateX(-1500px)';
+}
+btn[6].onclick =  function () {
+    slider.style.transform = 'translateX(-1800px)';
+}
+btn[7].onclick =  function () {
+    slider.style.transform = 'translateX(-2100px)';
+}
+btn[8].onclick =  function () {
+    slider.style.transform = 'translateX(-2400px)';
+}
+btn[9].onclick =  function () {
+    slider.style.transform = 'translateX(-2700px)';
+}
